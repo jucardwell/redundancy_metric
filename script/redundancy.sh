36434c458c95b7e5a2ef6ca264487263888d39b4
Julia Cardwell <91475282+jucardwell@users.noreply.github.com>
2026-01-20 09:44:32 -0500

    Update Slurm job parameters and R module version

--- script/redundancy.sh
+++ script/redundancy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #SBATCH -p general
 #SBATCH -N 1
-#SBATCH -t 00-02:00:00
-#SBATCH --mem=75g
+#SBATCH -t 00-01:20:00
+#SBATCH --mem=80g
 #SBATCH -n 1
 #SBATCH --job-name=redundancy
-#SBATCH --array=1-1980
+#SBATCH --array=1-7060
 
-# Load R module
-module load r/3.6.0
+#load R module
+module load r/4.4.0
 
-# Define R script path
+#define R script path
 R_SCRIPT="/work/users/j/m/jmcard/redundancy/script/redundancy.R"
 
-# Get the list of files in the folder
-FILES=($(ls /work/users/j/m/jmcard/redundancy/travel_matrices))
+#get the list of files in the folder
+FILES=($(TRAVEL_MATRIX_FOLDER))
 
-# Get the file corresponding to this Slurm task ID
+#get the file corresponding to this Slurm task ID
 FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
 
-# Run R script with the selected file as an argument
+#run R script with the selected file as an argument
 Rscript $R_SCRIPT $FILE
